29b33ad0e863c244e2f43e19360585a5afbda662
Vernon1999 <1193245595@qq.com>
2017-10-20 08:46:53 +0800

    1

---
+++ cssTM/TM.js
@@ -0,0 +1,94 @@
+{
+
+    let Top = document.querySelector("#fh");
+    let by=document.querySelector("body");
+    Top.onclick = function(){
+        document.documentElement.scrollTop = document.body.scrollTop =0;
+    }
+
+}
+
+// {
+//
+//     let dingbu=document.querySelector(".tbyc");
+//     let cel=document.querySelector(".cl");
+//     let e=document.querySelectorAll(".e");
+//     console.log(e);
+//     let liebiao=document.querySelectorAll(".cl .content");
+//     let zb=document.querySelector("#zhibo");
+//    window.onscroll=function(){
+//        let st=document.body.scrollTop===0? document.documentElement:document.body;
+//        let obj=st.scrollTop;
+//         if(obj>800){
+//             dingbu.style.top="0";
+//         }else{
+//             dingbu.style.top="-40px";
+//         }
+//     };
+//     window.addEventListener("scroll",function () {
+//         let st=document.body.scrollTop===0? document.documentElement:document.body;
+//         let obj=st.scrollTop;
+//         if(obj>800){
+//             cel.style.cssText="width:30px;height:180px";
+//         }else{
+//             cel.style.cssText="width:0;height:0";
+//         }
+//         for(let i=0;i<e.length;i++){
+//             if(obj>e[i].offsetTop){
+//                 for(let i=0;i<liebiao.length;i++){
+//                     liebiao[i].classList.remove("active");
+//                 }
+//                    liebiao[i].classList.add("active");
+//             }
+//         }
+//     });
+//
+//
+//     console.log(liebiao);
+//     window.addEventListener("scroll",function () {
+//         let st=document.body.scrollTop===0? document.documentElement:document.body;
+//         let obj=st.scrollTop;
+//         liebiao.forEach(function(ele,index) {
+//             ele.onclick=function () {
+//                 let ot=e[index].offsetTop;
+//                animate(st,{scrollTop:ot - 60},1000);
+//             }
+//         })
+//     });
+//
+//
+//
+//
+// }
+{
+    let cl=document.querySelector(".cl");
+    let ceh=document.querySelectorAll(".cl .content");
+    let neirong=document.querySelectorAll(".e");
+    let fc=document.querySelector(".tbyc");
+    console.log(neirong);
+    window.onscroll=function () {
+    let obj=document.body.scrollTop===0?document.documentElement:document.body;
+    let st=obj.scrollTop;
+    if(st>500){
+        cl.style.cssText="width:30px;height:180px";
+    }else{
+        cl.style.cssText="width:0;height:0";
+    };
+    if(st>500){
+        fc.style.top="0";
+    }else{
+        fc.style.top="-40px";
+    };
+    window.addEventListener("scroll",function () {
+        ceh.forEach=function(ele,index){
+            ele.onclick=function () {
+                
+            }
+        }
+    })
+
+    }
+
+
+
+}
